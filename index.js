d4d37301ab459bfba210903ce449f9cab61b392b
kikkia <sabledawn@gmail.com>
2022-11-24 00:38:27 -0800

    update selectors

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 const express = require("express");
 const login_url = "https://accounts.pixiv.net/login?return_to=https%3A%2F%2Fwww.pixiv.net%2Fen%2F&lang=en&source=pc&view_type=page";
-const user_selector = '#LoginComponent > form > div.input-field-group > div:nth-child(1) > input[type=text]';
-const pass_selector = '#LoginComponent > form > div.input-field-group > div:nth-child(2) > input[type=password]';
-const login_selector = '#LoginComponent > form > button';
+const user_selector = '#app-mount-point > div > div.sc-fvq2qx-4.idshsY > div.sc-2oz7me-0.DjdAN > div.sc-anyl02-2.joiqGm > div > div > div > form > fieldset.sc-bn9ph6-0.kJkgq.sc-2o1uwj-3.diUbPW > label > input';
+const pass_selector = '#app-mount-point > div > div.sc-fvq2qx-4.idshsY > div.sc-2oz7me-0.DjdAN > div.sc-anyl02-2.joiqGm > div > div > div > form > fieldset.sc-bn9ph6-0.kJkgq.sc-2o1uwj-4.hZIeVE > label > input';
+const login_selector = '#app-mount-point > div > div.sc-fvq2qx-4.idshsY > div.sc-2oz7me-0.DjdAN > div.sc-anyl02-2.joiqGm > div > div > div > form > button';
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 7788;
@@ -31,7 +31,6 @@ app.get('/login', async (req, res) => {
     var page = await browser.newPage();
 	await page.setUserAgent(userAgent.toString());
     await page.goto(login_url);
-    await page.waitForSelector(user_selector);
     await page.$eval(user_selector, (el, username) => {el.value = username;}, username);
     await page.$eval(pass_selector, (el, password) => {el.value = password;}, pass);
     await page.click(login_selector);
